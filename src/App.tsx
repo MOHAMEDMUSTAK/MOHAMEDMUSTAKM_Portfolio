408b893ef14ef396fd96cf22bd81de6593437b97
Mohamed Mustak M <mohamedmustak9355@gmail.com>
2026-05-03 23:10:01 +0530

    Switch to HashRouter for reliable GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AdminProvider } from "@/contexts/AdminContext";
 import { IntensityProvider } from "@/components/portfolio/IntensityToggle";
 import Index from "./pages/Index";
@@ -17,12 +17,12 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter basename="/MOHAMEDMUSTAKM_Portfolio">
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </TooltipProvider>
       </IntensityProvider>
     </AdminProvider>
