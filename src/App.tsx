4c1a1bd52389a0e73ce3f12d30100b7f277a59e9
Mohamed Mustak M <mohamedmustak9355@gmail.com>
2026-05-03 22:26:16 +0530

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/MOHAMEDMUSTAKM_Portfolio">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="*" element={<NotFound />} />
